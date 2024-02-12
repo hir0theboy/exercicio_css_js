754b7dcd0d602c02ddc5ec74af9b7fa0a81406ee
hir0theboy <lipealves2345meia78@gmail.com>
2024-02-11 23:20:18 -0300

    primeiro commit

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { CabecalhoStyle } from './styles'
+
+const Cabecalho = () => (
+  <CabecalhoStyle>
+    <h1>EBAC Jobs</h1>
+  </CabecalhoStyle>
+)
+
+export default Cabecalho
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from '../../global'
+import { DivContainer, Formulario, HeroTitle } from './styles'
+
+const Hero = () => (
+  <Formulario>
+    <Container>
+      <DivContainer>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </DivContainer>
+    </Container>
+  </Formulario>
+)
+
+export default Hero
